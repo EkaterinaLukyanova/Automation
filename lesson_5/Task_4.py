d14629c3d6fc8c43996947f5f4434f46e149c8ae
EkaterinaLukyanova <Lukyanovakaty86@gmail.com>
2024-06-23 23:18:06 +0300

    HW5

---
+++ lesson_5/Task_4.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ES
+
+try:
+   chrome = webdriver.Chrome()
+   firefox = webdriver.Firefox()
+   chrome.get("https://the-internet.herokuapp.com/entry_ad")
+   firefox.get("https://the-internet.herokuapp.com/entry_ad")
+   wait = WebDriverWait (chrome, 10)
+   wait = WebDriverWait (firefox, 10)
+   close_button = wait.until(ES.element_to_be_clickable((By.CSS_SELECTOR, "modal-footer")))
+   close_button.click()
+
+except Exception as ex:
+   print(ex)
+finally:
+   chrome.quit()
+   firefox.quit()
---
+++ lesson_5/Task_5.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+chrome = webdriver.Chrome()
+firefox = webdriver.Firefox()
+chrome.get("https://the-internet.herokuapp.com/inputs")
+firefox.get("https://the-internet.herokuapp.com/inputs")
+# Находим поле ввода по id и вводим текст "1000"
+input_field = chrome.find_element(By.TAG_NAME, "input")
+input_field = firefox.find_element(By.TAG_NAME, "input")
+input_field.send_keys("1000")
+# Очищаем поле ввода
+input_field.clear()
+# Вводим новый текст "999"
+input_field.send_keys("999")
+# Закрываем браузер
+chrome.quit()
+firefox.quit()
